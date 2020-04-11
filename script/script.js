6a367d7137d3eacf011336daeebaae1ee610cc2c
Luis Enrique <luisechvz@gmail.com>
2020-04-11 05:41:30 -0500

    Agregue que se puedan ingresar elementos en el hexagrama 1

--- script/script.js
+++ script/script.js
@@ -21,6 +21,8 @@ function guardarDato(){
         console.log("Valor Sumado igual a " + valorTotalDeLineaDeHexagrama);
         limpiarLosValores(primerInput,segundoInput,tercerInput);   /*Limpiamos los valores para que ingresen nuevos valores*/ 
         return valorTotalDeLineaDeHexagrama;
+    }else{
+        return false;
     }
 }
 
@@ -29,7 +31,7 @@ function validarValores(primerInput,segundoInput,tercerInput) {
     var expresionRegular = /^[2-3]{1}$/;
     
     if(expresionRegular.test(primerInput) && expresionRegular.test(segundoInput) && expresionRegular.test(tercerInput)){
-        alert("los valores son correctos"); 
+        //alert("los valores son correctos"); 
         return true;
     }else{
         alert("Los valores son incorrectos, VALORES VÁLIDOS(2,3)"); 
@@ -45,30 +47,25 @@ function limpiarLosValores(primerInput,segundoInput,tercerInput){
 
 //Sección 2 // METER LOS TEXTOS EN EL HEXAGRAMA
 
-/*Declaracion de lineas*/
-//el yin mutante sin x == yin
-//el yang mutante sin o == yang
-var yinMutanteConX,yinMutanteSinX_yin,yangMutanteConO,yangMutanteSinO_yang;
-    yinMutanteConX = document.createTextNode("-------X-------"); // valor de 6
-    yangMutanteConO = document.createTextNode("--------O---------"); // valor de 9
-    yinMutanteSinX_yin = document.createTextNode("------------------"); // valor de 7 o de 9 sin el 0
-    yangMutanteSinO_yang = document.createTextNode("---------  ---------"); // valor de 8 o de 6 sin la x
-
 var hexagramas = document.getElementsByClassName('hexagrama'); /*este es un array que almacena los 3 hexagramas*/
 var parrafoHexagrama1 = document.getElementsByClassName('parrafHexagrama1');
-
-var contadorDeParrafos = 6; /*Indicara que parrafo debe ser llenado*/
+var contadorDeParrafos = 5; /*Indicara que parrafo debe ser llenado*/
 
 /*Para el PRIMER HEXAGRAMA*/
 function insertarElementosEnElHexagramaUno(valorTotalDeLineaDeHexagrama){
     if(valorTotalDeLineaDeHexagrama == 6){
-        parrafoHexagrama1[contadorDeParrafos].appendChild(yinMutanteConX);
+        parrafoHexagrama1[contadorDeParrafos].innerHTML = '-------X-------'; // valor de 6
     }else if(valorTotalDeLineaDeHexagrama == 7){
-        parrafoHexagrama1[contadorDeParrafos].appendChild(yinMutanteSinX_yin);
+        parrafoHexagrama1[contadorDeParrafos].innerHTML = '----------------';  // valor de 7 o de 9 sin el 0
     }else if(valorTotalDeLineaDeHexagrama == 8){
-        parrafoHexagrama1[contadorDeParrafos].appendChild(yangMutanteSinO_yang);
+        parrafoHexagrama1[contadorDeParrafos].innerHTML = '--------  --------'; // valor de 8 o de 6 sin la x
     }else if(valorTotalDeLineaDeHexagrama == 9){
-        parrafoHexagrama1[contadorDeParrafos].appendChild(yangMutanteConO);
+        parrafoHexagrama1[contadorDeParrafos].innerHTML = '------O-------'; // valor de 9
+    }
+
+    //Aqui pondremos que tipo de Hexagrama es
+    if(contadorDeParrafos == 0){
+        parrafoHexagrama1[6].innerHTML = 'tipo' // valor de 9
     }
 
     contadorDeParrafos = contadorDeParrafos  - 1;
