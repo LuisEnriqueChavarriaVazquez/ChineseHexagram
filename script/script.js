c3924d15a467623bfaab447f6a38ffc6bbc10ebd
Luis Enrique <luisechvz@gmail.com>
2020-04-11 05:12:53 -0500

    Agregue algunas funciones para insertar texto con los valores las sumas

--- script/script.js
+++ script/script.js
@@ -1,7 +1,12 @@
+//Sección 1// VALIDAR LOS DATOS, SUMAR LOS DATOS Y LIMPIAR LOS IMPUTS
 /*Declaramos las variables para los valores de los inputs*/
 var primerInput,segundoInput,tercerInput;
-var valorTotalDeLineaDeHexagrama;
+var valorTotalDeLineaDeHexagrama; /*SUMA DE LOS VALORES*/
 
+function ejecutorDeFunciones(){
+    guardarDato();
+    insertarElementosEnElHexagramaUno(valorTotalDeLineaDeHexagrama);
+}
 
 function guardarDato(){
     primerInput = parseInt(document.getElementById('valorInput1').value, 10);
@@ -15,6 +20,7 @@ function guardarDato(){
         valorTotalDeLineaDeHexagrama = primerInput + segundoInput + tercerInput;
         console.log("Valor Sumado igual a " + valorTotalDeLineaDeHexagrama);
         limpiarLosValores(primerInput,segundoInput,tercerInput);   /*Limpiamos los valores para que ingresen nuevos valores*/ 
+        return valorTotalDeLineaDeHexagrama;
     }
 }
 
@@ -35,4 +41,36 @@ function limpiarLosValores(primerInput,segundoInput,tercerInput){
     document.getElementById('valorInput1').value = " ";
     document.getElementById('valorInput2').value = " ";
     document.getElementById('valorInput3').value = " ";
+}
+
+//Sección 2 // METER LOS TEXTOS EN EL HEXAGRAMA
+
+/*Declaracion de lineas*/
+//el yin mutante sin x == yin
+//el yang mutante sin o == yang
+var yinMutanteConX,yinMutanteSinX_yin,yangMutanteConO,yangMutanteSinO_yang;
+    yinMutanteConX = document.createTextNode("-------X-------"); // valor de 6
+    yangMutanteConO = document.createTextNode("--------O---------"); // valor de 9
+    yinMutanteSinX_yin = document.createTextNode("------------------"); // valor de 7 o de 9 sin el 0
+    yangMutanteSinO_yang = document.createTextNode("---------  ---------"); // valor de 8 o de 6 sin la x
+
+var hexagramas = document.getElementsByClassName('hexagrama'); /*este es un array que almacena los 3 hexagramas*/
+var parrafoHexagrama1 = document.getElementsByClassName('parrafHexagrama1');
+
+var contadorDeParrafos = 6; /*Indicara que parrafo debe ser llenado*/
+
+/*Para el PRIMER HEXAGRAMA*/
+function insertarElementosEnElHexagramaUno(valorTotalDeLineaDeHexagrama){
+    if(valorTotalDeLineaDeHexagrama == 6){
+        parrafoHexagrama1[contadorDeParrafos].appendChild(yinMutanteConX);
+    }else if(valorTotalDeLineaDeHexagrama == 7){
+        parrafoHexagrama1[contadorDeParrafos].appendChild(yinMutanteSinX_yin);
+    }else if(valorTotalDeLineaDeHexagrama == 8){
+        parrafoHexagrama1[contadorDeParrafos].appendChild(yangMutanteSinO_yang);
+    }else if(valorTotalDeLineaDeHexagrama == 9){
+        parrafoHexagrama1[contadorDeParrafos].appendChild(yangMutanteConO);
+    }
+
+    contadorDeParrafos = contadorDeParrafos  - 1;
+    return contadorDeParrafos;
 }
